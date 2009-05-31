45f7bf8fdf21bed228736ec3697c468a09c28d19
Josh Owens <joshua.owens@gmail.com>
2009-05-30 22:43:01 -0400

    Make sure we preserve the entire item.

--- lib/a2ws.rb
+++ lib/a2ws.rb
@@ -12,24 +12,46 @@ module A2WS
   end
 
   class ItemSearch < Base
+    attr_accessor :items
+    
     default_params :Operation => 'ItemSearch'
 
-    def initialize(api_key, search_index)
+    def initialize(api_key, search_index = :All)
       self.class.default_params :SearchIndex => search_index,  :AWSAccessKeyId => api_key
     end
 
-    def find(options = {})
-      result = self.class.get('/onca/xml', options)
+    def find(keywords, options = {})
+      options.merge!({:Keywords => keywords})
+      puts options.inspect
+      result = self.class.get('/onca/xml', :query => options)
 
-      items = result["ItemSearchResponse"]["Items"]
-      if items['Request']['IsValid'] == 'True'
-        items['Item'].collect do |i|
-          i['ItemAttributes'].inject({}) {|h, j| h[j.first.titlecase.downcase.gsub(' ', '_')] = j.last ; h}.to_mash
+      @items = result["ItemSearchResponse"]["Items"]
+      if @items['Request']['IsValid'] == 'True'
+        @items['Item'].collect do |i|
+          Mash.new downcase_keys(i)
         end
       else
-        raise items['Request']['Errors']['Error']['Message']
+        raise @items['Request']['Errors']['Error']['Message']
       end
     end
+    
+    protected
+    
+    def downcase_keys(hash)
+      new_hash = {}
+      hash.keys.each do |key|
+        value = hash.delete(key)
+        new_hash[downcase_key(key)] = value
+        new_hash[downcase_key(key)] = downcase_keys(value) if value.is_a?(Hash)
+        new_hash[downcase_key(key)] = value.each{|p| downcase_keys(p) if p.is_a?(Hash)} if value.is_a?(Array)
+      end
+      new_hash
+    end
+    
+    def downcase_key(key)
+      key.titlecase.downcase.gsub(' ', '_')
+    end
+    
   end
   
 end
