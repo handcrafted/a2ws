f5d2e553b3de68048bbab3e3b87dfb0ae5673f85
Andy Shen <andy@shenie.info>
2009-05-31 19:09:02 +1000

    Fix tests

--- spec/a2ws_spec.rb
+++ spec/a2ws_spec.rb
@@ -6,17 +6,21 @@ include A2WS
 config = YAML::load(open(ENV['HOME'] + '/.a2ws'))
 access_key = config["access_key"]
 
+A2WS::Base.configure do |config|
+  config.api_key = access_key
+end
+
 describe "A2WS Operations" do
 
   describe "ItemSearch" do
 
     it "should require SearchIndex" do
-      search = ItemSearch.new(access_key, nil)
-      lambda {search.find}.should raise_error
+      search = ItemSearch.new
+      lambda { search.find }.should raise_error
     end
 
     it "should find some items" do
-      search = ItemSearch.new(access_key, :Books)
+      search = ItemSearch.new(:Books)
       search.find(nil, :Title => 'Harry Potter').size.should_not == 0
     end
 
